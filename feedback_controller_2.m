f0c47f19415eeaba503e9c17c0930ec1ffbe9daa
Shuo Li <ls90911@gmail.com>
2019-08-06 11:11:39 +0200

    aa

---
+++ feedback_controller_2.m
@@ -0,0 +1,38 @@
+function [T] = feedback_controller_2(p_ref,v_ref,a_ref,states)
+
+k_p = 00.1;
+k_v = 0.1;
+
+g = 9.8;
+z_w = [0 0 1]';
+D = [-0.5 0 0; 0 -0.5 0; 0 0 -0.5];
+K_pos = [k_p 0 0;0 k_p 0;0 0 k_p];
+K_vel = [k_v 0 0;0 k_v 0;0 0 k_v];
+phi = states(7);
+theta = states(8);
+psi = states(9);
+
+R_E_B = [cos(theta)*cos(psi) cos(theta)*sin(psi) -sin(theta);...
+     sin(phi)*sin(theta)*cos(psi)-cos(phi)*sin(psi)...
+     sin(phi)*sin(theta)*sin(psi)+cos(phi)*cos(psi) sin(phi)*cos(theta);...
+     cos(phi)*sin(theta)*cos(psi)+sin(phi)*sin(psi)...
+     cos(phi)*sin(theta)*sin(psi)-sin(phi)*cos(psi) cos(phi)*cos(theta)];
+
+a_fb = K_pos * (p_ref-states(1:3)')+K_vel*(v_ref-states(4:6)');
+a_rd = R_E_B'*D*R_E_B*v_ref;
+
+a_des = a_ref + a_fb - a_rd - g*z_w;   % desired thrust vector
+
+z_b_des = a_des / norm(a_des);
+y_c = [-sin(psi) cos(psi) 0]';    % todo: maybe use desired psi instead of current psi
+x_b_des = cross(y_c,z_b_des)/ norm(cross(y_c,z_b_des));
+y_b_des = cross(z_b_des,x_b_des);
+T = a_des' * z_b_des;
+
+R = [x_b_des y_b_des z_b_des]';
+euler = rotm2eul(R);
+psi_des = euler(1);
+theta_des = euler(2);
+phi_des = euler(3);
+
+end
--- main_verify_rate_differential_flatness.m
+++ main_verify_rate_differential_flatness.m
@@ -1,7 +1,6 @@
 clear
 clc
 close all
-global log i states
 
 t0 = 0;
 tf = 10;
@@ -24,11 +23,14 @@ states = zeros((tf-t0)/time_step,9);
 x_ref = zeros((tf-t0)/time_step,1);
 y_ref = zeros((tf-t0)/time_step,1);
 z_ref = zeros((tf-t0)/time_step,1);
+v_x_ref = zeros((tf-t0)/time_step,1);
+v_y_ref = zeros((tf-t0)/time_step,1);
+v_z_ref = zeros((tf-t0)/time_step,1);
+a_x_ref = zeros((tf-t0)/time_step,1);
+a_y_ref = zeros((tf-t0)/time_step,1);
+a_z_ref = zeros((tf-t0)/time_step,1);
 psi_ref = zeros((tf-t0)/time_step,1);
 t = zeros((tf-t0)/time_step,1);
-% states(1,:) = [initial_constrains_x(1)-0.3 initial_constrains_y(1)+0.1 initial_constrains_z(1)-0.2...
-%     initial_constrains_x(2) initial_constrains_y(2) initial_constrains_z(2) 0 0 0];
-
 states(1,:) = [initial_constrains_x(1) initial_constrains_y(1) initial_constrains_z(1)...
     initial_constrains_x(2) initial_constrains_y(2) initial_constrains_z(2) 0 0 0];
 
@@ -40,20 +42,27 @@ for i = 1:(tf-t0)/time_step-1
         y_ref(i) = polyval(c_p_y,t(i));
         z_ref(i) = polyval(c_p_z,t(i));
         psi_ref(i) = polyval(c_p_psi,t(i));
+        v_x_ref(i) = polyval(c_v_x,t(i));
+        v_y_ref(i) = polyval(c_v_y,t(i));
+        v_z_ref(i) = polyval(c_v_z,t(i));
+        a_x_ref(i) = polyval(c_a_x,t(i));
+        a_y_ref(i) = polyval(c_a_y,t(i));
+        a_z_ref(i) = polyval(c_a_z,t(i));
     end
-%     log.t(i) = t(i);
-%     log.x_ref(i) = x_ref(i);
-%     log.y_ref(i) = y_ref(i);
-%     log.z_ref(i) = z_ref(i);
-    
     [angular_rate_ff,T_ff] = feed_forward_controller(c_v_x,c_v_y,c_v_z,...
                                                  c_a_x,c_a_y,c_a_z,...   
                                                  c_j_x,c_j_y,c_j_z,...
                                                  c_p_psi,c_v_psi,t(i));
-    [angular_rate_fb,T_fb] = feed_back_controller([x_ref(i) y_ref(i) z_ref(i)],...
-                                                               psi_ref(i),states(i,:),T_ff);
+%     [angular_rate_fb,T_fb] = feed_back_controller([x_ref(i) y_ref(i) z_ref(i)],...
+%                                                                psi_ref(i),states(i,:),T_ff);
 % [angular_rate_fb,T_fb] = feed_back_controller_pid([x_ref(i) y_ref(i) z_ref(i)],...
 %     psi_ref(i),states(i,:),T_ff);
+
+    [T] = feedback_controller_2([x_ref(i) y_ref(i) z_ref(i)]',...
+                                [v_x_ref(i) v_y_ref(i) v_z_ref(i)]',...
+                                [a_x_ref(i) a_y_ref(i) a_z_ref(i)]',...
+                                states(i,:));
+                            
     angular_rate = angular_rate_ff + angular_rate_fb;
     T = T_ff + T_fb;
     states(i+1,:) =  states(i,:) + time_step * drone_model(states(i,:),[angular_rate' T])';
@@ -63,9 +72,14 @@ for i = 1:(tf-t0)/time_step-1
     y_ref(i+1) = polyval(c_p_y,t(i+1));
     z_ref(i+1) = polyval(c_p_z,t(i+1));
     psi_ref(i+1) = polyval(c_p_psi,t(i+1));
+    v_x_ref(i+1) = polyval(c_v_x,t(i+1));
+    v_y_ref(i+1) = polyval(c_v_y,t(i+1));
+    v_z_ref(i+1) = polyval(c_v_z,t(i+1));
+    a_x_ref(i+1) = polyval(c_a_x,t(i+1));
+    a_y_ref(i+1) = polyval(c_a_y,t(i+1));
+    a_z_ref(i+1) = polyval(c_a_z,t(i+1));
 end
 
-%debug_plot();
 
 figure(1)
 subplot(3,1,1)
